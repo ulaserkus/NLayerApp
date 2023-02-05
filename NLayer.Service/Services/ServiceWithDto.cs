da73a8d384f4d10304651fc0bb7105cd18929ce3
Ulaş Erkuş <ulas.erkus@omerd.com>
2023-02-05 15:46:09 +0300

    Generic Service interfaces added to retrun response dto

---
+++ NLayer.Service/Services/ServiceWithDto.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using NLayer.Core;
+using NLayer.Core.DTOs;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class ServiceWithDto<Entity, Dto> : IServiceWithDto<Entity, Dto>
+        where Entity : BaseEntity
+        where Dto : class
+    {
+        private readonly IGenericRepository<Entity> _repository;
+        protected readonly IUnitOfWork _unitOfWork;
+        protected readonly IMapper _mapper;
+
+        public ServiceWithDto(IGenericRepository<Entity> repository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<CustomResponseDto<Dto>> AddAsync(Dto dto)
+        {
+            Entity entity = _mapper.Map<Entity>(dto);
+            await _repository.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            var newDto = _mapper.Map<Dto>(entity);
+            return CustomResponseDto<Dto>.Success(StatusCodes.Status200OK, newDto);
+        }
+
+        public async Task<CustomResponseDto<IEnumerable<Dto>>> AddRangeAsync(IEnumerable<Dto> dtos)
+        {
+            List<Entity> entities = _mapper.Map<List<Entity>>(dtos);
+            await _repository.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+            var newDtos = _mapper.Map<List<Dto>>(entities);
+            return CustomResponseDto<IEnumerable<Dto>>.Success(StatusCodes.Status200OK, newDtos);
+        }
+
+        public Task<CustomResponseDto<bool>> AnyAsync(Expression<Func<Entity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponseDto<IEnumerable<Dto>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponseDto<Dto>> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponseDto<NoContentDto>> RemoveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponseDto<NoContentDto>> RemoveRangeAsync(IEnumerable<int> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponseDto<NoContentDto>> UpdateAsync(Dto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponseDto<IEnumerable<Dto>>> Where(Expression<Func<Entity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
